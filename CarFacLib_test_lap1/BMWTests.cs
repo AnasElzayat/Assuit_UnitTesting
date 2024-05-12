f1f90e28e510b064d490f4a995b8b2f6fe570f02
Anas <anasashrafelzayat@gmail.com>
2024-05-12 14:00:20 +0200

    lab1

---
+++ CarFacLib_test_lap1/BMWTests.cs
@@ -0,0 +1,88 @@
+﻿using CarFactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFacLib_test_lap1
+{
+    public class BMWTests
+    {
+        [Fact]
+        public void IsStopped_velocity20_false()
+        {
+            BMW bmw = new BMW();
+            bmw.velocity = 20;
+
+            bool result = bmw.IsStopped();
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IncreaseVelocity_valocity20Add20_40()
+        {
+            BMW bmw = new BMW();
+            bmw.velocity = 20;
+            double AddedVelocitoy = 20;
+
+            
+            bmw.IncreaseVelocity(AddedVelocitoy);
+
+            
+            Assert.Equal(40, bmw.velocity);
+            Assert.InRange(bmw.velocity, 20, 60); 
+        }
+
+        [Fact]
+        public void GetDirection_DirectionStopped_Stopped()
+        {
+            BMW bmw = new BMW();
+            bmw.drivingMode = DrivingMode.Stopped;
+
+            
+            string result = bmw.GetDirection();
+
+            
+            Assert.Equal(DrivingMode.Stopped.ToString(), result);
+
+            Assert.StartsWith("S", result);
+            Assert.EndsWith("ed", result);
+
+            Assert.Contains("pp", result);
+            Assert.DoesNotContain("aaa", result);
+
+            
+        }
+        [Fact]
+        public void GetDirection_DirectionBackward_Backward()
+        {
+            BMW bmw = new BMW();
+            bmw.drivingMode = DrivingMode.Backward;
+
+
+            string result = bmw.GetDirection();
+
+
+            Assert.Equal(DrivingMode.Backward.ToString(), result);
+
+            Assert.StartsWith("B", result);
+            
+
+
+        }
+
+        [Fact]
+        public void GetMyCar_callFunction_SameCar()
+        {
+            BMW bmw = new BMW();
+
+            
+            Car car = bmw.GetMyCar();
+
+            
+            Assert.Same(bmw, car); 
+        }
+    }
+}
---
+++ CarFacLib_test_lap1/CarFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using CarFactoryLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFacLib_test_lap1
+{
+    public class CarFactoryTests
+    {
+        [Fact]
+        public void NewCar_CarTypeToyota_Toyota()
+        {
+            
+            Car? car = CarFactory.NewCar(CarTypes.Toyota);
+
+            Assert.IsType<Toyota>(car);
+
+        }
+
+        [Fact]
+        public void NewCar_CarTypeHonda_Exception()
+        {
+            Assert.Throws<NotImplementedException>(() =>
+            {
+                Car? result = CarFactory.NewCar(CarTypes.Kia);
+            });
+        }
+    }
+}
